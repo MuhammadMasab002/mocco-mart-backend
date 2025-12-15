203c7ec20dc4de39143e0ec9cbc5e3eca4425ae7
MuhammadMasab002 <muhammadmasab2002@gmail.com>
2025-12-15 21:01:11 +0500

    cart model

---
+++ src/models/cart.models.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+
+const cartSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    items: [
+      {
+        product: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        // variant: {
+        //   type: Schema.Types.ObjectId,
+        //   ref: "Variant",
+        // },
+        quantity: Number,
+        price: Number,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Cart = mongoose.model("Cart", cartSchema);

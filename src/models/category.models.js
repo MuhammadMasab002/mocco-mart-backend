2e11ec2dbe93623b794c5a3bf54208a0239efaf7
MuhammadMasab002 <muhammadmasab2002@gmail.com>
2025-12-17 23:56:22 +0500

    category model - enhance schema structure

--- src/models/category.models.js
+++ src/models/category.models.js
@@ -2,22 +2,28 @@ import mongoose, { Schema } from "mongoose";
 
 const categorySchema = new Schema(
   {
-    name: { type: String, required: true },
-    description: { type: String, required: false },
-    isActive: { type: Boolean, default: true },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    parent: {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+      default: null, // null = main category
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
   },
   { timestamps: true }
 );
-
-// Virtual Populate
-categorySchema.virtual("products", {
-  ref: "Product",
-  localField: "_id",
-  foreignField: "categoryId",
-});
-
-// Include virtuals in JSON response
-categorySchema.set("toObject", { virtuals: true });
-categorySchema.set("toJSON", { virtuals: true });
 
 export const Category = mongoose.model("Category", categorySchema);
